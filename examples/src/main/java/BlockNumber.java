51314158be1bd25c6f985535dedf800789942596
quan8 <quan.nguyen@fantom.foundation>
2022-02-22 09:21:16 +1100

    update example

--- examples/src/main/java/BlockNumber.java
+++ examples/src/main/java/BlockNumber.java
@@ -7,9 +7,9 @@ import java.util.concurrent.ExecutionException;
 
 public class BlockNumber {
     public static void main(String[] args) throws ExecutionException, InterruptedException{
-        Web3j web3 = Web3j.build(new HttpService("https://rpc.fantom.network"));
+        Web3j web3 = Web3j.build(new HttpService("http://127.0.0.1:18545"));
         EthBlockNumber result = web3.ethBlockNumber().sendAsync().get(); 
-        System.out.println(" The Block Number is: " + result.getBlockNumber().toString());
+        System.out.println("The Block Number is: " + result.getBlockNumber().toString());
         System.exit(0);
     }
 }
